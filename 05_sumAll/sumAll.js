f6c95f18b9c0f2c34a932414a35cea731ae3161e
John Rabe <johnmrabe@gmail.com>
2023-06-06 15:12:56 +0800

    Update JS with answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (n, end) {
+  if (n < 0 || end < 0) return "ERROR";
+  if (!Number.isInteger(n) || !Number.isInteger(end)) return "ERROR";
+  if (n > end) {
+    let temp = n;
+    n = end;
+    end = temp;
+  }
+  for (sum = 0; n < end + 1; n++) {
+    sum += n;
+  }
+  return sum;
 };
 
 // Do not edit below this line
